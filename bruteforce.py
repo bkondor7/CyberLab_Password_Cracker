51f66e7f59c93b805763c382b21aa3eaf12fb45c
Bohdan Kondor <bohkondo@gmail.com>
2024-08-04 22:07:52 -0400

    Task solution

--- bruteforce.py
+++ bruteforce.py
@@ -5,24 +5,25 @@ Bruteforce starter template
 
 from zipfile import ZipFile
 
-# Use a method to attempt to extract the zip file with a given password
-# def attempt_extract(zf_handle, password):
-#     
-#
-#
+def attempt_extract(zf_handle, password):
+    try:
+        zf_handle.extractall(pwd=password.encode('utf-8'))
+        return True
+    except (RuntimeError):
+        return False
 
 def main():
     print("[+] Beginning bruteforce ")
     with ZipFile('enc.zip') as zf:
         with open('rockyou.txt', 'rb') as f:
-            # Write your logic here...
-            # Iterate through password entries in rockyou.txt
-
-            # Attempt to extract the zip file using each password
-
-            # Handle correct password extract versus incorrect password attempt)
-
-    #print("[+] Password not found in list")
+            for line in f:
+                password = line.strip().decode('utf-8')
+                if attempt_extract(zf, password):
+                    print(f"[+] Password found: {password}")
+                    return
+                else:
+                    print(f"[-] Attempted password: {password} - Incorrect")
+    print("[+] Password not found in list")
 
 if __name__ == "__main__":
     main()
